623c11e678228145bcc76d7fffd4bbb5e629991c
aradhna7 <aradhna.7sharma@gmail.com>
2021-01-13 20:30:34 +0530

    login and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import './App.css';
+import Signin from './components/auth/Signin'
+import Signup from './components/auth/Signup';
+
 
 const App = () => {
   return (
-    <div className="App">
-      helllooo
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+    
+    </Router>
   );
 }
 
---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import photo from '../../images/login.png'
+
+const Signup = () => {
+    return (
+        <div className="container-login100">
+        <div className="wrap-login100">
+        <div className="login-img">
+            <img src={photo} alt="IMG"/>
+        </div>
+        
+    
+        <form className="login100-form validate-form">
+            <span className="login100-form-title">
+                Signup
+            </span>
+            <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                
+                
+            </div>
+            <div className="wrap-input100 validate-input" data-validate="Password is required">
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                <i className="fa fa-lock" aria-hidden="true"></i>
+                </span>
+            </div>
+            <div className="container-login100-form-btn">
+                <button className="btn btn-dark btn-block">
+                    Signup
+                </button>
+            </div>
+            <br />
+            <div className="text-center p-t-136">
+                <Link to="/signin">
+                    Already have an account. Signin now!!
+                </Link>
+            </div>
+        </form>
+        </div>
+        </div>
+    )
+}
+
+export default Signup
